1109e2bc2d78993493fd907efd4f5e1fc926e291
GALANTE Giovanni <giovanni.galante@soprasteria.com>
2022-12-19 09:15:54 +0100

    esempio di discovery server Eureka

---
+++ src/main/java/com/soprasteria/discovery/DiscoveryApplication.java
@@ -0,0 +1,15 @@
+package com.soprasteria.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer	//IL DISCOVERY
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+
+}
